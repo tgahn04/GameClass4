9f0886b02fec8e47a8948b3460c6523d785a1988
tgahn04 <tgahn0524@gmail.com>
2025-10-16 19:40:44 +0900

    (2025.10.16) 이중 연결 리스트

--- Progrem/Progrem.cpp
+++ Progrem/Progrem.cpp
@@ -8,92 +8,33 @@ class List
 private:
 	struct Node
 	{
+		Node* previous;
 		T data;
 		Node* next;
 	};
 
 	int size;
+
 	Node* head;
+	Node* tail;
 
 public:
 	List()
 	{
 		size = 0;
 		head = nullptr;
+		tail = nullptr;
 	}
 
-	void push_front(T data)
-	{
-		Node* newNode = new Node;
 
-		newNode->data = data;
 
-		if (head == nullptr)
-		{
-			head = newNode;
-
-			newNode->next = nullptr;
-		}
-		else
-		{
-			newNode->next = head;
-
-			head = newNode;
-		}
-		size++;
-	}
-
-	void pop_front()
-	{
-		if (head == nullptr)
-		{
-			cout << "Linked List is Empty" << endl;
-			return;
-		}
-
-		Node* deleteNode = head;
-
-		head = head->next;
-
-		delete deleteNode;
-
-		size--;
-	}
-
-	void push_back(T data)
-	{
-		Node* newNode = new Node;
-		newNode->data = data;
-		newNode->next = nullptr;
-		
-		Node* currentNode = head;
-
-		while (currentNode->next != nullptr)
-		{
-			currentNode = currentNode->next;
-		}
-
-		currentNode->next = newNode;
-
-		size++;
-	}
 };
-
 
 int main()
 {
 	List<int> list;
 
-	list.push_front(10);
-	list.push_front(5);
 
-	list.pop_front();
-	list.pop_front();
-	list.pop_front();
-
-	list.push_back(10);
-	list.push_back(20);
-	list.push_back(30);
 
 	return 0;
 }
