cc141019fb635939f73300e2b84834c265c532c8
tgahn04 <tgahn0524@gmail.com>
2025-10-24 19:45:55 +0900

    (2025.10.24) 우선순위 큐

---
+++ Class6th (Linear Queue)/Source.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define SIZE 5
+
+template <typename T>
+class Queue
+{
+private:
+    int front;
+    int rear;
+
+    T container[SIZE];
+
+public:
+    Queue()
+    {
+        front = 0;
+        rear = 0;
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            container[i] = NULL;
+        }
+    }
+
+    void push(T data)
+    {
+        if (rear >= SIZE)
+        {
+            cout << "linear_queue_overflow" << endl;
+            return;
+        }
+        else
+        {
+            container[rear++] = data;
+        }
+    }
+
+    void pop()
+    {
+        if (empty())
+        {
+            cout << "linear_queue_is_empty" << endl;
+            exit(1);
+        }
+        else
+        {
+            container[front++] = NULL;
+        }
+    }
+
+    const bool& empty()
+    {
+        return (front == rear);
+    }
+
+    const T peek()
+    {
+        if (empty())
+        {
+            exit(1);
+        }
+        else
+        {
+            return container[front];
+        }
+    }
+
+    const int& size()
+    {
+        return rear - front;
+    }
+};
+
+int main()
+{
+    Queue<int> queue;
+
+    queue.push(10);
+    queue.push(20);
+    queue.push(30);
+    queue.push(40);
+    queue.push(50);
+    queue.push(60);
+
+    while (queue.empty() == false)
+    {
+        cout << queue.peek() << endl;
+
+        queue.pop();
+    }
+
+    cout << "Queue Size : " << queue.size() << endl;
+
+    queue.push(60);
+
+    return 0;
+}
--- Progrem/Progrem.cpp
+++ Progrem/Progrem.cpp
@@ -2,22 +2,22 @@
 
 using namespace std;
 
-#define SIZE 5
+#define SIZE 4
 
 template <typename T>
 class Queue
 {
 private:
-    int front;
     int rear;
+    int front;
 
     T container[SIZE];
 
 public:
     Queue()
     {
-        front = 0;
-        rear = 0;
+        front = SIZE - 1;
+        rear = SIZE - 1;
 
         for (int i = 0; i < SIZE; i++)
         {
@@ -27,46 +27,69 @@ public:
 
     void push(T data)
     {
-        if (rear == SIZE)
+        if (front == (rear + 1) % SIZE)
         {
-            cout << "linear queue overflow" << endl;
-            return;
+            cout << "Circular_queue_overflow" << endl;
         }
-        container[rear++] = data;
+        else
+        {
+            rear = (rear - 1 + SIZE) % SIZE;
+            container[rear] = data;
+        }
     }
 
     void pop()
     {
         if (empty())
         {
+            cout << "Circular_queue_is_empty" << endl;
             exit(1);
         }
-        for (int i = 0; i < rear - 1; i++)
+        else
         {
-            container[i] = container[i + 1];
-        }
-        container
+            front = (front + 1) % SIZE;
 
+            container[front] = NULL;
+        }
     }
 
     const bool& empty()
     {
-        bool emp = false;
-        emp = (front == rear);
-        return emp;
+        return (front == rear);
     }
 
     const T peek()
     {
-
+        if (empty())
+        {
+            exit(1);
+        }
+        else
+        {
+            return container[(front + 1) % SIZE];
+        }
     }
 };
+
 
 int main()
 {
     Queue<int> queue;
 
+    queue.push(10);
+    queue.push(20);
+    queue.push(30);
 
+    while (queue.empty() == false)
+    {
+        cout << queue.peek() << endl;
+
+        queue.pop();
+    }
+
+    queue.push(40);
+    queue.push(50);
+    queue.push(60);
 
     return 0;
 }
