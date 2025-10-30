de14c223ad9454db9beeffd13cde7abfc1bc64e1
tgahn04 <tgahn0524@gmail.com>
2025-10-30 19:46:29 +0900

    (2025.10.30) 인접 그래프

---
+++ Class9th (Hash Table)/Source.cpp
@@ -0,0 +1,167 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename KEY, typename VALUE>
+class HashTable
+{
+private:
+    struct Node
+    {
+        KEY key;
+        VALUE value;
+
+        Node* next;
+    };
+
+    struct Bucket
+    {
+        int count;
+        Node* head;
+    };
+
+    int size;
+    int capacity;
+
+    Bucket* bucket;
+
+public:
+    HashTable()
+    {
+        size = 0;
+        capacity = 8;
+
+        bucket = new Bucket[capacity];
+
+        for (int i = 0; i < capacity; i++)
+        {
+            bucket[i].head = nullptr;
+            bucket[i].count = 0;
+        }
+    }
+
+    template<typename KEY>
+    unsigned int hash_function(KEY key)
+    {
+        return (unsigned int)key % capacity;
+    }
+
+    template<>
+    unsigned int hash_function(const char* key)
+    {
+        int sum = 0;
+
+        for (int i = 0; *key != '\0'; i++)
+        {
+            sum += key[i];
+
+            key = key + 1;
+        }
+
+        return (unsigned int)sum % capacity;
+    }
+
+    void insert(KEY key, VALUE value)
+    {
+        // 해시 함수를 통해서 값을 받는 임시 변수
+        int hashIndex = hash_function(key);
+
+        // 새로운 노드를 생성합니다.
+        Node * newNode = new Node;
+
+        newNode->key = key;
+        newNode->value = value;
+        newNode->next = nullptr;
+
+        // 노드가 1개라도 존재하지 않는다면
+        if (bucket[hashIndex].count == 0)
+        {
+            // bucket[hashIndex]의 head 포인터가 newNode를 가리키게 됩니다.
+            bucket[hashIndex].head = newNode;
+        }
+        else
+        {
+            newNode->next = bucket[hashIndex].head;
+
+            bucket[hashIndex].head = newNode;
+        }
+
+        // bucket[hashIndex]의 count를 증가시킵니다.
+        bucket[hashIndex].count++;
+
+        size++;
+    }
+
+    void erase(KEY key)
+    {
+        // 1. 해시 함수를 통해서 값을 받는 임시 변수
+        int hashIndex = hash_function(key);
+
+        // 2. Node를 탐색할 수 있는 포인터 변수 선언
+        Node* currentNode = bucket[hashIndex].head;
+
+        // 3. 이전 Node를 저장할 수 있는 포인터 변수 선언
+        Node* previousNode = nullptr;
+
+        // 4. currentNode가 nullptr과 같다면 함수를 종료합니다.
+        if (currentNode == nullptr)
+        {
+            cout << "not_key_found..." << endl;
+
+            return;
+        }
+        else
+        {
+            while (currentNode != nullptr)
+            {
+                if (currentNode->key == key)
+                    break;
+
+                previousNode = currentNode;
+                currentNode = currentNode->next;
+            }
+
+            if (currentNode != nullptr)
+            {
+                if (previousNode == nullptr)
+                {
+                    bucket[hashIndex].head = currentNode->next;
+                }
+                else
+                {
+                    previousNode->next = currentNode->next;
+                }
+
+                delete currentNode;
+                bucket[hashIndex].count--;
+                size--;
+            }
+            else
+            {
+                cout << "not_key_found..." << endl;
+            }
+        }
+    }
+};
+
+int main()
+{
+    HashTable<const char*, int> hashtable;
+
+    // cout << hashtable.hash_function("Ko115frea") << endl;
+    // cout << hashtable.hash_function("rqwr") << endl;
+    // cout << hashtable.hash_function("Chirteyna") << endl;
+    // cout << hashtable.hash_function("C5qdqdyna") << endl;
+    // cout << hashtable.hash_function("C6wrrwrwyna") << endl;
+    // cout << hashtable.hash_function("Chi868na") << endl;
+
+    hashtable.insert("Abyssal Mask", 3000);
+    hashtable.insert("Bami's Cinder", 1000);
+    hashtable.insert("Chain Vast", 800);
+
+    hashtable.erase("Abyssal Mask");
+    hashtable.erase("Bami's Cinder");
+    hashtable.erase("Frozen Heart");
+
+    return 0;
+}
--- Progrem/Progrem.cpp
+++ Progrem/Progrem.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-template <typename KEY, typename VALUE>
+template<typename KEY, typename VALUE>
 class HashTable
 {
 private:
@@ -10,13 +10,14 @@ private:
     {
         KEY key;
         VALUE value;
+
         Node* next;
     };
 
     struct Bucket
     {
-        Node* head;
         int count;
+        Node* head;
     };
 
     int size;
@@ -35,16 +36,142 @@ public:
         for (int i = 0; i < capacity; i++)
         {
             bucket[i].head = nullptr;
+            bucket[i].count = 0;
         }
     }
 
+    template<typename KEY>
+    unsigned int hash_function(KEY key)
+    {
+        return (unsigned int)key % capacity;
+    }
 
+    template<>
+    unsigned int hash_function(const char* key)
+    {
+        int sum = 0;
+
+        for (int i = 0; *key != '\0'; i++)
+        {
+            sum += key[i];
+
+            key = key + 1;
+        }
+
+        return (unsigned int)sum % capacity;
+    }
+
+    void insert(KEY key, VALUE value)
+    {
+        // 해시 함수를 통해서 값을 받는 임시 변수
+        int hashIndex = hash_function(key);
+
+        // 새로운 노드를 생성합니다.
+        Node* newNode = new Node;
+
+        newNode->key = key;
+        newNode->value = value;
+        newNode->next = nullptr;
+
+        // 노드가 1개라도 존재하지 않는다면
+        if (bucket[hashIndex].count == 0)
+        {
+            // bucket[hashIndex]의 head 포인터가 newNode를 가리키게 됩니다.
+            bucket[hashIndex].head = newNode;
+        }
+        else
+        {
+            newNode->next = bucket[hashIndex].head;
+
+            bucket[hashIndex].head = newNode;
+        }
+
+        // bucket[hashIndex]의 count를 증가시킵니다.
+        bucket[hashIndex].count++;
+
+        size++;
+    }
+
+    void erase(KEY key)
+    {
+        // 1. 해시 함수를 통해서 값을 받는 임시 변수
+        int hashIndex = hash_function(key);
+
+        // 2. Node를 탐색할 수 있는 포인터 변수 선언
+        Node* currentNode = bucket[hashIndex].head;
+
+        // 3. 이전 Node를 저장할 수 있는 포인터 변수 선언
+        Node* previousNode = nullptr;
+
+        // 4. currentNode가 nullptr과 같다면 함수를 종료합니다.
+        if (currentNode == nullptr)
+        {
+            cout << "not_key_found..." << endl;
+
+            return;
+        }
+        else
+        {
+            // 5. currentNode를 이용해서 내가 찾고자 하는 key값을 찾습니다.
+            while (currentNode != nullptr)
+            {
+                if (currentNode->key == key)
+                {
+                    if (currentNode == bucket[hashIndex].head)
+                    {
+                        bucket[hashIndex].head = currentNode->next;
+                    }
+                    else
+                    {
+                        previousNode->next = currentNode->next;
+                    }
+
+                    size--;
+                    bucket[hashIndex].count--;
+                    delete currentNode;
+
+                    return;
+                }
+                else
+                {   
+                    previousNode = currentNode;
+                    currentNode = currentNode->next;
+                }
+            }
+        }
+    }
+
+    const float& load_factor()
+    {
+        return (float)size / capacity;
+    }
+
+    const int& bucket_count()
+    {
+        return capacity;
+    }
 };
 
 int main()
 {
-    HashTable<int, string> hashtable;
+    HashTable<const char*, int> hashtable;
 
+    // cout << hashtable.hash_function("Ko115frea") << endl;
+    // cout << hashtable.hash_function("rqwr") << endl;
+    // cout << hashtable.hash_function("Chirteyna") << endl;
+    // cout << hashtable.hash_function("C5qdqdyna") << endl;
+    // cout << hashtable.hash_function("C6wrrwrwyna") << endl;
+    // cout << hashtable.hash_function("Chi868na") << endl;
+
+    hashtable.insert("Abyssal Mask", 3000);
+    hashtable.insert("Bami's Cinder", 1000);
+    hashtable.insert("Chain Vast", 800);
+
+    hashtable.erase("Abyssal Mask");
+    hashtable.erase("Frozen Heart");
+
+    cout << "Load_Factor : " << hashtable.load_factor() << endl;
+    cout << "Bucket_Count : " << hashtable.bucket_count() << endl;
 
     return 0;
 }
